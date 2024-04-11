20be158b166511069c5e4379ecd44a1b2c130962
ken1iro <kenichiro.okayasu.k361@dirbato.co.jp>
2024-04-11 14:56:40 +0900

    チャプター3,4

---
+++ Test/src/calc/CalculatorTestTest.java
@@ -0,0 +1,19 @@
+package calc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTestTest {
+
+    @Test
+    void testTestAdd() {
+        fail("まだ実装されていません");
+    }
+
+    @Test
+    void testTestSubtract() {
+        fail("まだ実装されていません");
+    }
+
+}
